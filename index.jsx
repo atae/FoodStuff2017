66852e61fd18a3d54a3571fa263303345f098931
Andrew Tae <andrew.tae@M305190KAG8WP.community.veritas.com>
2017-07-08 03:23:06 -0700

    Bootstrap Working

--- index.jsx
+++ index.jsx
@@ -14,23 +14,51 @@ var events ={
 }
 
 $.get('restaurants.json', function(res) {
-  console.log(res)
 })
 
 // console.log(restaurants)
 
 
-
-document.addEventListener("DOMContentLoaded", function() {
-  var Title = React.createClass({
-    render: function() {
-      return (
+var Title = React.createClass({
+  render: function() {
+    return (
       <div>
-        <h1> It's React Time. </h1>
+      <DayCounter/>
+      <h1> It's React Time. Whoa.</h1>
       </div>
     )
+  }
+})
+
+var DayCounter = React.createClass({
+  getDays: function() {
+    var deadline = new Date(2017, 21, 0, 0, 0)
+    var lastDay = new Date(2017, 24, 0, 0, 0)
+    var currentDay = new Date()
+    var message = []
+    if (currentDay < deadline) {
+        var daysLeft = parseInt(deadline.date - currentDay.date)
+        return(<h2> Only <h1>{this.daysLeft}</h1> days left! </h2>)
+      )
+    } elseif (currentDay > lastDay) {
+        return(<h2> That's it! Go <h1>Home</h1>! </h2>)
+    } else {
+        var day = parseInt(lastDay.date - currentDay.date)
+        return(<h2> Day <h1>{this.day}</h1></h2>)
     }
-  })
+
+  },
+  render: function() {
+    var timeContent = this.getDays()
+    return (
+      <div>
+        {this.timeContent}
+      </div>
+    )
+  }
+})
+
+document.addEventListener("DOMContentLoaded", function() {
   // var schedule = document.getElementById('schedule')
   // for (var i = 0; i < events.day1.places.length; i++) {
   //   var location = document.createElement("LI")
@@ -38,7 +66,6 @@ document.addEventListener("DOMContentLoaded", function() {
   //   schedule.appendChild(location)
   // }
   var app = document.getElementById('app')
-  console.log(app);
   ReactDOM.render(<Title />, app)
 
 })
